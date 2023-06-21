8fd4ed8721bfceb8295287482c2ac41c84c48f76
ayoubleass <ayoubleas@gmail.com>
2023-06-21 11:24:44 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char string[] = "_putchar";
+int size = sizeof(string);
+int  i  = 0;
+while (i < size)
+{
+_putchar(string[i]);
+}
+return (0);
+}
