6fdc4b8f6ff5d48f35dfd09418aeb4563d5a355a
ayoubleass <ayoubleas@gmail.com>
2023-06-21 12:34:24 -0700

    tast 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,9 @@
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success) 
- * print_alphabet
- *  Return: Always 0 (Success) 
- */
-int print_alphabet(void);
 
 int main(void)
 {
-print_alphabet();
-return (0);
+return print_alphabet();
 }
 
 
