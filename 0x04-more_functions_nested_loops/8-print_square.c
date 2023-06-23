955de87711e356e0371040de6c800a91e5861675
ayoubleass <ayoubleas@gmail.com>
2023-06-23 05:15:35 -0700

    task 7 ,8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,14 @@ void print_square(int size)
 	int row, column;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
 		return;
+	}
 	for (column = 1; column <= size; column++)
 	{
 		for (row = 1; row <= size; row++)
 			_putchar('#');
 		_putchar('\n');
+	}
 }
