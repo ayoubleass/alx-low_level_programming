035800397d57d955189a0ce10327d60d2e92ddba
ayoubleass <ayoubleas@gmail.com>
2023-06-29 08:18:12 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,9 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; src[i] != "\0" ; i++)
 	{
-		dest[destLength + i] = src[i];
+		dest += src[i];
 	}
-	dest[dest_len + i] = '\n';
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
