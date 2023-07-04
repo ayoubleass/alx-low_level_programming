fcc434743e8f804673dfaec460a69f91f6f38801
ayoubleass <ayoubleas@gmail.com>
2023-07-04 03:50:53 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,30 +15,13 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
-	char *matches = haystack;
+	int i;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
-			matches = &haystack[i];
-
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-				{
-					matches = haystack;
-					break;
-				}
-			}
-			if (needle[j] == '\0')
-			{
-				return (matches);
-			}
-
-
-
+			return (&haystack[i]);
 		}
 
 
