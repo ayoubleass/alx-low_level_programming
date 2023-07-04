7ee0dcb2a6407159884bfc440ecc834ea9b66213
ayoubleass <ayoubleas@gmail.com>
2023-07-04 03:07:19 -0700

    update 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -20,10 +20,10 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != c)
+		if (s[i] == c)
 		{
-			return (NULL);
+			return (&s[i]);
 		}
-		return (&s[i]);
 	}
+	return (NULL);
 }
