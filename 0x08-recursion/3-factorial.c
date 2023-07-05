902877a8c9e6c74bacc652620bdcc76a42a2d58a
ayoubleass <ayoubleas@gmail.com>
2023-07-05 08:16:31 -0700

    fixing requirement error File: 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,9 +8,16 @@
 
 int factorial(int n)
 {
-	if (n <= 0)
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
 	{
 		return (1);
 	}
-	return (n * factorial(n - 1));
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
