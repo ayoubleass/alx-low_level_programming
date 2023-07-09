0312bd8c955ef05a0e3f2940d4f37627cd6e095b
ayoubleass <ayoubleas@gmail.com>
2023-07-09 09:05:30 -0700

    File: 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, 1 if an error occurred
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		char *current = argv[i];
+
+		while (*current)
+		{
+			int asciiValue = (int)*current;
+
+			if (asciiValue < 48 || asciiValue > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			current++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
