98cce5a7adf723e4ea24773698ae165daff82f88
ayoubleass <ayoubleas@gmail.com>
2023-07-11 01:13:30 -0700

    File: 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,10 @@ char *str_concat(char *s1, char *s2)
 	char *arr;
 	int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size1 = sizeOfString(s1);
 	size2 = sizeOfString(s2);
 
