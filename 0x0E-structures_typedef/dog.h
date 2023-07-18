f4f5dcb2b5e219ea98ef9457a326884c51bf5f8d
ayoubleass <ayoubleas@gmail.com>
2023-07-18 04:09:44 -0700

    File: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog -structer datatype
+*@name:string
+*@age:foat
+*@owner:string
+ */
+
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
