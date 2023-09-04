a03126140203438f128347a82c106b95e7f3388a
ayoubleass <ayoubleas@gmail.com>
2023-09-04 08:44:49 -0700

    File: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,6 +16,11 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr;
 
+	if (width == 0 || height == 0)
+	{
+		return  (NULL);
+	}
+
 	arr = malloc(height * sizeof(int *));
 
 	if (arr == NULL)
