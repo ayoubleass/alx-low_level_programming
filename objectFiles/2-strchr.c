ebe384fbcd8f972204636889e1102a8aedc30289
ayoubleass <ayoubleas@gmail.com>
2023-09-07 12:14:22 -0700

    File: 2-calloc.c

---
+++ objectFiles/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ * @s: The string to search in.
+ * @c: The character to locate.
+ *
+ * Return: Pointer to the first occurrence of the character in the string,
+ *         or NULL if the character is not found.
+ */
+
+
+
+
+
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
---
+++ objectFiles/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to a null-terminated string.
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
