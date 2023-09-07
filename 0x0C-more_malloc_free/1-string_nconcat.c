eb059de68958ac223e305ca4a24b6e0a9eb240d5
ayoubleass <ayoubleas@gmail.com>
2023-09-07 11:15:10 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * strLength - return the size of a string .
+ * @s: the first element of the passed string
+ * @n: index
+ * Return: the size of a string
+ */
+
+int strLength(char *s, int n)
+{
+	if (s[n] == '\0')
+	{
+		return (n);
+	}
+	return (strLength(s, n + 1));
+}
+
+/**
+ * string_nconcat - Concatenates two strings up to n characters.
+ * @s1: The first input string.
+ * @s2: The second input string.
+ * @n: The maximum number of characters from s2 to concatenate.
+ *
+ * Return: A pointer to the newly allocated concatenated string,
+ * or NULL on failure.
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *result;
+	int i, j;
+
+	int s1Length = strLength(s1, 0);
+	int s2Length = strLength(s2, 0);
+	int size = s1Length + n;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	if (n >= s2Length)
+		int size = s1Length + s2Length;
+
+	result = malloc(size + 1);
+
+	if (result == NULL)
+		return (NULL);
+
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (j < n)
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	return (result);
+}
