f3a62338e7c646e73f60b0a1ddeb6dbece73711e
ayoubleass <ayoubleas@gmail.com>
2023-09-22 19:09:49 -0700

    File :100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+void __constructor(void);
+
+void __constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
