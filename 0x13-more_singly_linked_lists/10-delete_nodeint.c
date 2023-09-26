04452deb76f066dc5e3c828212ec54de35927608
ayoubleass <ayoubleas@gmail.com>
2023-09-25 22:35:46 -0700

    File: 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current, *prev;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -22,7 +22,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		pop_listint(head);
+		*head = current->next;
+		free(current);
 		return (1);
 	}
 
