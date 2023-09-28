a01d3767276a1da35de978f565cde28a9effd65c
ayoubleass <ayoubleas@gmail.com>
2023-09-28 11:37:12 -0700

    File: 2-get_bit.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_binary -  prints the binary representation of a number.
+ * @n: unsigned long integer to be printed in binary.
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int allowed = 0;
+	int len = sizeof(unsigned long int) * 4;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	i = len;
+	while (i >= 0)
+	{
+		unsigned long int mask = (1 << i);
+
+		if (n & mask)
+		{
+			printf("1");
+			allowed = 1;
+		}
+		else if (allowed)
+		{
+			printf("0");
+		}
+		i--;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef BIT_MANIPULATION_H
 #define BIT_MANIPULATION_H
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
