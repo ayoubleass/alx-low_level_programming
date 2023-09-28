ef3528cf7b5d36d3c769947ae0c122b0e91e0025
ayoubleass <ayoubleas@gmail.com>
2023-09-28 00:47:54 -0700

    File: 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: stirng to convert
+  * Return: unsigned int in base 10
+  */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int len = strlen(b);
+	int i = len - 1;
+	unsigned int cover = 1;
+
+	if (len == 1 && b[0] == 1)
+	{
+		result = 1;
+		return (result);
+	}
+
+	while (i >= 0)
+	{
+		if (b[i] == '1')
+			result += cover;
+		if (b[i] != '0' &&  b[i] != '1')
+			return (0);
+		cover *= 2;
+		i--;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef BIT_MANIPULATION_H
+#define BIT_MANIPULATION_H
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
