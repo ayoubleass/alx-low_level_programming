ccdb13c269c396c249c685a917f74343429fd9bc
ayoubleass <ayoubleas@gmail.com>
2023-10-02 18:20:12 -0700

    File: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_EXCL | O_RDWR, 0666);
 
 	if (fd == -1)
-	{
-		close(fd);
 		return (result);
-	}
 
 	if (text_content != NULL)
 	{
