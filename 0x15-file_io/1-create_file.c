ad4edb4856e007e6a4d93e3d2cb3ed1008eae810
ayoubleass <ayoubleas@gmail.com>
2023-10-02 18:35:10 -0700

    File: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,11 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	fd = open(filename, O_CREAT, 0666);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0600);
 	if (fd == -1)
 		return (result);
-
 	if (text_content != NULL)
 	{
 		size = write(fd, text_content, strlen(text_content));
@@ -33,7 +31,6 @@ int create_file(const char *filename, char *text_content)
 			return (result);
 		}
 	}
-
 	close(fd);
 	return (1);
 }
