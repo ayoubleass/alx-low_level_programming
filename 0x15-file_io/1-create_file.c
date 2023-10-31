f7702c0fae4935c140c48d8ed5bc0aba449935e4
ayoubleass <ayoubleas@gmail.com>
2023-10-31 03:49:27 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,12 +38,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (len > 0)
+	if (len)
 		size = write(fd, text_content, len);
 	close(fd);
 	if (len != size)
 		return (-1);
-
 	return (1);
 }
 
