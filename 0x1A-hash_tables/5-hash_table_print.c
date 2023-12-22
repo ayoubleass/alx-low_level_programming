b336031bf9b653122ac05405530500c66384ae78
ayoubleass <ayoubleas@gmail.com>
2023-12-22 05:22:09 -0800

    File: 3-hash_table_set.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,6 +14,8 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i = 0;
 	int printed  = 0;
 
+	if (ht == NULL)
+		return;
 	printf("{");
 	while (i < ht->size)
 	{
@@ -23,7 +25,7 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			printed = 1;
-		}	
+		}
 		i++;
 	}
 	printf("}\n");
