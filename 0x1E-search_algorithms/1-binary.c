5ddd7ae9f411597bb0389d3a899201e49b18079f
ayoubleass <ayoubleas@gmail.com>
2024-04-12 03:23:26 -0700

    fixing task 02

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,7 +12,7 @@ void printArray(int *arr, size_t l, size_t h)
 {
 	size_t second = l + 1;
 
-	printf("Searching in array:");
+	printf("Searching in array: ");
 	while (l <= h)
 	{
 		if (l >= second)
@@ -23,9 +23,25 @@ void printArray(int *arr, size_t l, size_t h)
 	}
 	printf("\n");
 }
+/**
+ * isInArray - check if the value is in the array.
+ * @array: Pointer to the first elemnt of the array
+ * @size: the size of the array
+ * @value: the value to search for
+ * Return: 0if found else -1
+ */
 
+int isInArray(int *array, size_t size, int value)
+{
+	size_t i = 0;
 
-
+	while (i < size)
+	{
+		if (array[i] == value)
+			return (0);
+	}
+	return (-1);
+}
 /**
  * binary_search - Searches for a value in a sorted array of
  * integers using the binary search algorithm.
@@ -44,7 +60,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t l = 0, h = size - 1;
 	int mid = 0;
 
-	if (array == NULL)
+	if (array == NULL && isInArray(array, size, value) == -1)
 		return (-1);
 
 	while (l <= h)
